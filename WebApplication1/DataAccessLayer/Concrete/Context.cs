b198dc7e12c0dfa1d400ea2d7870a964bb78665c
MustafaNur <mustafa@drupart.co.uk>
2023-08-14 19:19:30 +0300

    Interfaceler ve Generic Servisler

---
+++ WebApplication1/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-ABE0UME; database=CoreProjeDB; integrated security=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> portfolios { get; set; }
+        public DbSet<Service> services { get; set; }
+        public DbSet<Skill> skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonials> Testimonials { get; set; }
+    }
+}
