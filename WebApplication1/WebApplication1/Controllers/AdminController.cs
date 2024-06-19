d0986a627d3ca91046c19881bb8ecf68625805d6
MustafaNur <mustafa@drupart.co.uk>
2024-06-19 16:09:31 +0300

    Admin

--- WebApplication1/WebApplication1/Controllers/AdminController.cs
+++ WebApplication1/WebApplication1/Controllers/AdminController.cs
@@ -8,5 +8,21 @@ namespace SuperFolio.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
--- WebApplication1/WebApplication1/Controllers/SkillController.cs
+++ WebApplication1/WebApplication1/Controllers/SkillController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SuperFolio.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            var values = skillManager.GetList();
             return View();
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
