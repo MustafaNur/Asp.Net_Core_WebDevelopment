2cec3f2d26ce5bc005b7af26bdccc5148f002a9d
MustafaNur <mustafa@drupart.co.uk>
2024-06-20 00:44:18 +0300

    Admin

--- WebApplication1/WebApplication1/Controllers/SkillController.cs
+++ WebApplication1/WebApplication1/Controllers/SkillController.cs
@@ -10,18 +10,46 @@ namespace SuperFolio.Controllers
         SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
             var values = skillManager.GetList();
-            return View();
+            return View(values);
         }
         [HttpGet]
         public IActionResult AddSkill()
         {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
             return View();
         }
         [HttpPost]
         public IActionResult AddSkill(Skill skill)
         {
             skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.TGetByID(id);
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Yetenek Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Düzenleme";
+            var values = skillManager.TGetByID(id);
+            skillManager.TUpdate(values);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
             return RedirectToAction("Index");
         }
     }
